e0d9a595a2bd02d4d181aad9800df82c023730e1
Justxd22 <xdjust18@gmail.com>
2024-04-30 01:08:45 +0300

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.engine.db_storage import classe
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
+    """Status endpoint."""
     return jsonify(status="OK")
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Stats endpoint."""
+    c = classe
+    for x in c:
+        c[x] = storage.count(c[x])
+    return jsonify(c)
