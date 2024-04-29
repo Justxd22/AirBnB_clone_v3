7823c3e8390cef93b06b2ff16195db051e90e6dd
Justxd22 <xdjust18@gmail.com>
2024-04-30 01:11:49 +0300

    ddd

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Init.
+"""
+
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Init.
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
